b058faecc4dea84dfb879d0f4cca918c633ce586
CodeWithAyaN01 <ayankrrish@gmail.com>
2026-02-15 13:15:17 +0530

    added Text to Speech

--- X_bott/frontend/src/App.jsx
+++ X_bott/frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Pad from './components/Pad'
 const App = () => {
   return (
     <div className='bg-black min-h-screen w-full'>
-      <Navbar text='Welcome ' />
+      <Navbar text='Welcome to PointX' />
       <div>
         <Pad />
       </div>
